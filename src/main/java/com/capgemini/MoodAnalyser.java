21572733f5a3bb1bd976eec11cfb0d5ca6e3784a
n-ndini7 <hp@RJs-PC>
2020-09-30 18:58:10 +0530

    [nandini] added master - welcome message

---
+++ src/main/java/com/capgemini/MoodAnalyser.java
@@ -0,0 +1,8 @@
+package com.capgemini;
+
+//master branch - welcome message
+public class MoodAnalyser {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Mood Analyser Program!!");
+	}
+}
