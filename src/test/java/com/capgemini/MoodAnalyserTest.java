adbc148d44c70462b3135e32019dea58c0f5283e
n-ndini7 <hp@RJs-PC>
2020-09-30 22:53:32 +0530

    [nandini] added TC3.1 - custom moodanalyserexception

--- src/test/java/com/capgemini/MoodAnalyserTest.java
+++ src/test/java/com/capgemini/MoodAnalyserTest.java
@@ -1,5 +1,6 @@
 package com.capgemini;
 
+import static org.junit.Assert.*;
 import org.hamcrest.CoreMatchers;
 import org.junit.*;
 import org.junit.Test;
@@ -9,12 +10,17 @@ public class MoodAnalyserTest {
 	@Test
 	public void moodAnalysis() throws NullPointerException {
 		MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-		// for null input 
-		String mood = moodAnalyser.analyseMood();
-		ExpectedException x = ExpectedException.none();
-		x.expect(NullPointerException.class);
-		Assert.assertEquals("HAPPY", mood);
+
 	}
 
+	@Test
+	public void moodAnalysis_throws_nullException() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+		try {
+			moodAnalyser.analyseMood();
+		} catch (MoodAnalyserException e) {
+			assertEquals(MoodAnalyserException.ExceptionType.ENTERED_NULL, e.type);
+		}
+	}
 
 }
